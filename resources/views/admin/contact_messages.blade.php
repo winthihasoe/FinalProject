a0b857266b77444e8b1545bce0e8d7d03f261002
winthihasoe <winthihasoe@gmial.com>
2020-12-03 22:16:45 +0630

    create tables for admin control panel

--- resources/views/admin/contact_messages.blade.php
+++ resources/views/admin/contact_messages.blade.php
@@ -1,4 +1,26 @@
 @extends('layouts.adminlayout')
 @section('content')
-    <h1>Contact Messages</h1>
+    <h2>Contact Messages</h2>
+        <table class="table table-hover">
+    <thead class="grey">
+        <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Username</th>
+        <th scope="col">Email</th>
+        <th scope="col">Messages</th>       
+        <th scope="col">Update</th>
+        <th scope="col">Delete</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <th>1</th>
+        <td>Mark</td>
+        <td>god@gmail.com</td>
+        <td>Hello Admin, how are you?</td>
+        <td><a href=""><button class="btn btn-sm mt-0 green">Update</button></a></td>
+        <td><a href=""><button class="btn btn-sm mt-0 red">Delete</button></a></td>
+        </tr>
+  </tbody>
+</table>
 @endsection
--- resources/views/admin/manage_premium_users.blade.php
+++ resources/views/admin/manage_premium_users.blade.php
@@ -1,4 +1,28 @@
 @extends('layouts.adminlayout')
 @section('content')
-    <h1>Manage Premium Users</h1>
+    <h2>Manage Premium Users</h2>
+    <table class="table table-hover">
+    <thead class="grey">
+        <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Username</th>
+        <th scope="col">Email</th>
+        <th scope="col">isAdmin</th>
+        <th scope="col">isPremium</th>
+        <th scope="col">Update</th>
+        <th scope="col">Delete</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <th>1</th>
+        <td>Mark</td>
+        <td>god@gmail.com</td>
+        <td>true</td>
+        <td>true</td>
+        <td><a href=""><button class="btn btn-sm mt-0 green">Update</button></a></td>
+        <td><a href=""><button class="btn btn-sm mt-0 red">Delete</button></a></td>
+        </tr>
+  </tbody>
+</table>
 @endsection
