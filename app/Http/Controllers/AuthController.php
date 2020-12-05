847c8c10b3fb5494eb5586db707785c0d8ad497e
winthihasoe <winthihasoe@gmial.com>
2020-12-06 00:39:32 +0630

    save register data and image name to DB

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -17,24 +19,30 @@ class AuthController extends Controller
     function post_register(){
         // authentication 
         $validation=request()->validate([
-            // 'username'=>'required',
-            // 'email'=>'required',
-            // 'password'=>'required || min:8',
+            'username'=>'required',
+            'email'=>'required',
+            'password'=>'required || min:8',
             'image'=>'required'
         ]);
 
         if($validation){
+            // save to public folder
             $image=request('image'); //save to public folder
             $image_name=uniqid()."_".$image->getClientOriginalName(); //save to db table as a new image name with random id
-
             $image->move(public_path('images/profiles'),$image_name);
             
-            // return $image_name;
-            // dd($image);
+            // save to user db table
+            $password=$validation['password'];
+            $user=new User();
+            $user->name=$validation['username'];
+            $user->email=$validation['email'];
+            $user->password=Hash::make($password);
+            $user->image=$image_name;
+            $user->save();
             
-            // return redirect()->route('home');
-        // }else{
-        //     return back()->withErrors($validation);
+            return redirect()->route('home');
+        }else{
+            return back()->withErrors($validation);
         }
     }
 }
