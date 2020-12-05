d18b5c90eb8c8f9472b0cee200a1e64dad4d19ff
winthihasoe <winthihasoe@gmial.com>
2020-12-06 00:00:28 +0630

    get image name and move to public_path

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,16 +17,24 @@ class AuthController extends Controller
     function post_register(){
         // authentication 
         $validation=request()->validate([
-            'username'=>'required',
-            'email'=>'required',
-            'password'=>'required || min:8',
+            // 'username'=>'required',
+            // 'email'=>'required',
+            // 'password'=>'required || min:8',
             'image'=>'required'
         ]);
 
         if($validation){
-            return redirect()->route('home');
-        }else{
-            return back()->withErrors($validation);
+            $image=request('image'); //save to public folder
+            $image_name=uniqid()."_".$image->getClientOriginalName(); //save to db table as a new image name with random id
+
+            $image->move(public_path('images/profiles'),$image_name);
+            
+            // return $image_name;
+            // dd($image);
+            
+            // return redirect()->route('home');
+        // }else{
+        //     return back()->withErrors($validation);
         }
     }
 }
