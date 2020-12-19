9ca29be5b753c3a186062559c98daf0c037c8b76
winthihasoe <winthihasoe@gmial.com>
2020-12-20 00:32:30 +0630

    using database magic relation method

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -7,32 +7,36 @@
     {{-- card --}}
     <div class="container">
         <h1 class="mt-4">All Posts</h1>
-        <div class="row">
+        <div class="row d-flex">
         @foreach ($posts as $post)
-            <div class="col-md-4 mt-3">
+            <div class="col-md-4 mt-3 d-flex">
                 <!-- Card -->
-                <div class="card">
+               
 
-                    <!-- Card image -->
-                    <div class="view overlay">
-                        <img class="card-img-top" src="{{asset('images/posts/'.$post->image)}}" alt="">
-                        <a href="{{route('postById',$post->id)}}">
-                            <div class="mask rgba-white-slight"></div>
-                        </a>
+                    <div class="card">
+
+                        <!-- Card image -->
+                        <div class="view overlay">
+                            <img class="card-img-top" src="{{asset('images/posts/'.$post->image)}}" alt="">
+                            <a href="{{route('postById',$post->id)}}">
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                        </div>
+                    
+                        <!-- Card content -->
+                        <div class="card-body">
+                    
+                            <!-- Title -->
+                            <h4 class="card-title">{{$post->title}}</h4>
+                            <p>(posted by {{$post->user->name}})</p>
+                        
+                            <!-- Button -->
+                            <a href="{{route('postById',$post->id)}}" class="btn btn-primary">See More</a>
+                    
+                        </div>
+                    
                     </div>
-                
-                    <!-- Card content -->
-                    <div class="card-body">
-                
-                        <!-- Title -->
-                        <h4 class="card-title">{{$post->title}}</h4>
-                       
-                        <!-- Button -->
-                        <a href="{{route('postById',$post->id)}}" class="btn btn-primary">See More</a>
-                
-                    </div>
-                
-                </div>
+             
                 <!-- Card -->
                 
 
